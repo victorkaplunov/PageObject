05187fa09a0f09f6ab95267f453a4f764efe3b05
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-07 21:05:44 +0300

    add login page tests realization. /lesson/238819/step/7

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,22 +1,28 @@
 from selenium.common.exceptions import NoSuchElementException
 
 
-class BasePage(object):
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
-
-    def open(self):
-        self.browser.get(self.url)
-
+class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
 
+    def open(self):
+        self.browser.get(self.url)
+
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def is_string_in_url_present(self, url_substring):
+        print(self.browser.current_url)
+        print("Подстрока в URL? ", url_substring in self.browser.current_url)
+        # url_substring in self.browser.current_url
+        try:
+            url_substring in self.browser.current_url
         except NoSuchElementException:
             return False
         return True
---
+++ pages/locators.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+
+
+class MainPageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, '#registration_link')
+
+
+class LoginPageLocators:
+    LOGIN_PAGE_URL = ('dsfdfs')
+    LOGIN_FORM = (By.CSS_SELECTOR, '#id_login-username')
+    REGISTER_FORM = (By.CSS_SELECTOR, '#id_registration-email')
+
