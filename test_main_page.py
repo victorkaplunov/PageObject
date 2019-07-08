53636b3cebe319c59952f3db290de3326428a2c7
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-08 11:23:06 +0300

    /lesson/238819/step/8

--- test_main_page.py
+++ test_main_page.py
@@ -2,18 +2,18 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-#     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-#     page.open()  # открываем страницу
-#     page.go_to_login_page()  # выполняем метод страницы -- переходим на страницу логина
-#
-#
-# def test_guest_should_see_login_link(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы -- переходим на страницу логина
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 
 def test_guest_should_be_login_page(browser):
@@ -21,5 +21,5 @@ def test_guest_should_be_login_page(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
-    # page.should_be_login_form()
-    # page.should_be_register_form()
+    page.should_be_login_form()
+    page.should_be_register_form()
