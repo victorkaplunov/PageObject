8c4d19feb2c3d5df7b390ed28590a37bd835c17a
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-08 11:26:40 +0300

    /lesson/238819/step/8

--- pages/locators.py
+++ pages/locators.py
@@ -1,13 +1,12 @@
 from selenium.webdriver.common.by import By
 
 
-
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, '#registration_link')
 
 
 class LoginPageLocators:
-    LOGIN_PAGE_URL = ('dsfdfs')
+    LOGIN_PAGE_URL = '/login/'
     LOGIN_FORM = (By.CSS_SELECTOR, '#id_login-username')
     REGISTER_FORM = (By.CSS_SELECTOR, '#id_registration-email')
-
+    
