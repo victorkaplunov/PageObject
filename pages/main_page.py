cd10494a5ce8b29bb082175f7c9a602fd11e8753
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-08 11:24:53 +0300

    /lesson/238819/step/8

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .login_page import LoginPage
 from .locators import MainPageLocators
 
 
@@ -6,6 +7,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not present"
