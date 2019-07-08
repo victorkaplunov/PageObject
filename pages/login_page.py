387a9d7971420492dcfca5eb23f86a037fd6002f
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-08 11:25:56 +0300

    /lesson/238819/step/8

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,19 +5,17 @@ from .locators import LoginPageLocators
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
-        # self.should_be_login_form()
-        # self.should_be_register_form()
+        self.should_be_login_form()
+        self.should_be_register_form()
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        # print(LoginPageLocators.LOGIN_PAGE_URL)
-        print("Assert: ", self.is_string_in_url_present(LoginPageLocators.LOGIN_PAGE_URL))
         assert self.is_string_in_url_present(LoginPageLocators.LOGIN_PAGE_URL), "URL do not contain 'login' string."
 
-    # def should_be_login_form(self):
-    #     # реализуйте проверку, что есть форма логина
-    #     assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login name form is not present"
-    #
-    # def should_be_register_form(self):
-    #     # реализуйте проверку, что есть форма регистрации на странице
-    #     assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register e-mail form is not present"
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login name form is not present"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register e-mail form is not present"
