0505588f63a860938349e70bd83006929148498c
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-08 19:38:00 +0300

    /lesson/201964/step/4

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,7 +4,43 @@ from selenium.common.exceptions import NoSuchElementException
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
+        self.url = urlimport math
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
+
+
+class BasePage:
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def is_string_in_url_present(self, url_substring):
+        return url_substring in self.browser.current_url
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            print("Your code: {}".format(alert.text))
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
         self.browser.implicitly_wait(timeout)
 
     def open(self):
