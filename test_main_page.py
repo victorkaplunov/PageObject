3b5c475cbdb553671b15316c3ba3698e0971b70c
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-09 16:58:41 +0300

    add lesson/201964/step/10

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,6 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
-
+import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
@@ -23,3 +23,12 @@ def test_guest_should_be_login_page(browser):
     page.should_be_login_url()
     page.should_be_login_form()
     page.should_be_register_form()
+
+
+def test_guest_cant_see_product_in_cart_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_not_be_goods_in_basket()
+    page.basket_is_empty_message_present()
