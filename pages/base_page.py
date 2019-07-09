ab8e5fbe81b951210dbe6cd38f56b4e50225ec06
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-09 09:51:48 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,4 @@
-from selenium.common.exceptions import NoSuchElementException
-
-
-class BasePage:
-    def __init__(self, browser, url, timeout=10):
-        self.browser = browser
-        self.url = urlimport math
+import math
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import NoSuchElementException
 
@@ -40,18 +34,3 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
-
-        self.browser.implicitly_wait(timeout)
-
-    def open(self):
-        self.browser.get(self.url)
-
-    def is_element_present(self, how, what):
-        try:
-            self.browser.find_element(how, what)
-        except NoSuchElementException:
-            return False
-        return True
-
-    def is_string_in_url_present(self, url_substring):
-        return url_substring in self.browser.current_url
