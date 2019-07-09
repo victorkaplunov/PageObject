87e91c95db10ab4831ec63146108cd3b35652fa3
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-09 17:01:48 +0300

    add lesson/201964/step/10

--- pages/locators.py
+++ pages/locators.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 class BasePageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, '#default > header > div.page_inner > div > div.basket-mini.pull-right.hidden-xs > span > a')
 
 
 class MainPageLocators:
@@ -28,3 +29,9 @@ class ProductPageLocators:
     BOOK_PRICE = (By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/p[1]')
     MESSAGE_BOOK_PRICE = (By.XPATH, '//*[@id="messages"]/div[3]/div/p[1]/strong')
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div')
+
+
+class BasketPageLocators:
+    """locators for product page"""
+    BASKET_CONTENT = (By.CSS_SELECTOR, '#basket_formset > div')
+    BASKET_IS_EMPTY_MESSAGE = (By.XPATH, '//*[@id="content_inner"]/p')
