760ddd75b740993a9189eb7bb961ac4c935fa6f8
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-09 16:57:58 +0300

    add lesson/201964/step/10

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 """Tests for product page"""
 import pytest
 from .pages.product_page import ProductPage
-
+import time
 
 @pytest.mark.parametrize('link', [
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -63,3 +63,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
     page.should_be_login_url()
+
+
+def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_not_be_goods_in_basket()
+    page.basket_is_empty_message_present()
