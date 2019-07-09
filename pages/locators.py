a232e35e540ec2a346357014bc6cd72dc1cf9cd3
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-09 15:07:19 +0300

    add lesson/201964/step/8

--- pages/locators.py
+++ pages/locators.py
@@ -2,6 +2,11 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
 class MainPageLocators:
     """locators for main page"""
     LOGIN_LINK = (By.CSS_SELECTOR, '#registration_link')
@@ -22,3 +27,4 @@ class ProductPageLocators:
     MESSAGE_BOOK_TITLE = (By.XPATH, '//*[@id="messages"]/div[1]/div/strong')
     BOOK_PRICE = (By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/p[1]')
     MESSAGE_BOOK_PRICE = (By.XPATH, '//*[@id="messages"]/div[3]/div/p[1]/strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div')
