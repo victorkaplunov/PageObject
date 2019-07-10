071c293e41af648c1993ffe6b903e795d5d4d8f1
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-10 16:36:01 +0300

    /lesson/201964/step/14

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,12 @@
 """Tests for product page"""
 import pytest
 from .pages.product_page import ProductPage
-import time
+from .pages.login_page import LoginPage
+from .pages.cart_page import BasketPage
+import faker
 
+
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', [
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -11,7 +15,8 @@ import time
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                 marks=pytest.mark.xfail(reason="some persistent bug")),
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
 ])
@@ -27,6 +32,7 @@ def test_guest_can_add_product_to_cart(browser, link):
     page.compare_book_price(page.get_book_price_from_message())
 
 
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
@@ -42,6 +48,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_cart(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
@@ -57,6 +64,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -65,10 +73,43 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.should_be_login_url()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket()
+    page = BasketPage(browser, link)
     page.should_not_be_goods_in_basket()
     page.basket_is_empty_message_present()
+
+
+class TestUserAddToCartFromProductPage(object):
+    @pytest.fixture(autouse=True)
+    def setup(self, browser):
+        fake = faker.Faker()
+        email = fake.email()
+        password = fake.password(length=9)
+        link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(email, password)
+
+    def test_user_cant_see_success_message(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_cart(self, browser):
+        """Go to page with promo 'newYear' and add book to basket.
+        In alert window insert data. Compare title and price in message
+        and book description. We are register a user before test."""
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        page.compare_book_title(page.get_book_title_from_message())
+        page.compare_book_price(page.get_book_price_from_message())
